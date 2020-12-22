42a6cbf963dde49ca4e9c77af5a6dde4e4404d1a
Antoni Mcyr <xiberian@icloud.com>
2020-12-23 01:25:11 +0300

    removed unnecessary abstraction

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import { askQuestion, print } from './cli.js';
 
-const greetPlayer = async () => {
-  print('Welcome to the Brain Games');
-  const name = await askQuestion('May I have a name, sir? ');
-  print(`Hello, ${name}!`);
-  return name;
-};
-
 export default async (description, startRound) => {
-  const playerName = await greetPlayer();
+  print('Welcome to the Brain Games');
+  const playerName = await askQuestion('May I have a name, sir? ');
+  print(`Hello, ${playerName}!`);
+
   print(description);
   const roundsToWin = 3;
   let correctAnswersCount = 0;
